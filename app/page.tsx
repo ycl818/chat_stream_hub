cc64a9a88547afda1400f59c697b68963b6a51b8
yichien <kevinyichienlee@gmail.com>
2023-09-04 21:57:08 +0800

    taste of cn function

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+let state = true;
 
 export default function Home() {
   return (
     <div className="">
       <p className="text-3xl font-bold text-indigo-500 ">Hello</p>
-      <Button variant="destructive">Click me</Button>
+      <Button className={cn("bg-indigo-500", state && "bg-red-500")}>
+        Click me
+      </Button>
     </div>
   );
 }
