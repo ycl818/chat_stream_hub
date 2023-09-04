ac2500da9b8a12526243ee3725fb2ec64855578f
yichien <kevinyichienlee@gmail.com>
2023-09-04 21:45:52 +0800

    add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="text-3xl font-bold text-align-500 ">Hello </p>;
+  return (
+    <div className="">
+      <p className="text-3xl font-bold text-indigo-500 ">Hello</p>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
 }
