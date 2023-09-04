1e7095d5ffcdd3af24a15af73d0fc8b61af13522
yichien <kevinyichienlee@gmail.com>
2023-09-04 23:38:45 +0800

    add clerk auth

--- app/(auth)/login/page.tsx
+++
@@ -1,5 +0,0 @@
-const LoginPage = () => {
-  return <div>loginPage</div>;
-};
-
-export default LoginPage;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div className="">This is a protected route.</div>;
+  return (
+    <div className="">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
