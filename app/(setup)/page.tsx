c30e8302b53e108d26fe653643ce0a8e74c6dd72
yichien <kevinyichienlee@gmail.com>
2023-09-08 00:25:08 +0800

    add ui for modal, and initial-modal ing

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -9,18 +10,18 @@ const SetupPage = async () => {
   const server = await db.server.findFirst({
     where: {
       members: {
-        some:{
-          profileId: profile.id
-        }
-      }
-    }
-  })
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
