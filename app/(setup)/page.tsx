c58dd027579ce91b33b8672d6fac956da2f2882e
yichien <kevinyichienlee@gmail.com>
2023-09-07 23:48:52 +0800

    export user profile func

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  // find this person in which server
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some:{
+          profileId: profile.id
+        }
+      }
+    }
+  })
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div>Create a server</div>;
+};
+
+export default SetupPage;
