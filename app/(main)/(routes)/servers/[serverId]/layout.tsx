98876390ea5d93af940155c72098b3087d45540a
yichien <kevinyichienlee@gmail.com>
2023-09-10 13:48:16 +0800

    create server-sidebar layout

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,44 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { serverId: string };
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 flex-col fixed inset-y-0">
+        Server sidebar
+      </div>
+      <div className="h-full md:pl-60">{children}</div>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
