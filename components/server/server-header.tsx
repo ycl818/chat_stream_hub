001a7829b4b1a5b0f5cd2c43be804035a838e553
yichien <kevinyichienlee@gmail.com>
2023-09-10 15:02:08 +0800

    implement server sidebar

---
+++ components/server/server-header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ServerWithMembersWithProfiles } from "@/types";
+import { MemberRole, Server } from "@prisma/client";
+
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfiles;
+  role?: MemberRole;
+}
+
+export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  return <div>Server Header</div>;
+};
