7fb683370f40a11351f7aab0ae6b739e152bd6be
ycl818 <kevinyichienlee@gmail.com>
2023-09-18 10:38:27 +0800

    finished search component

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -10,6 +10,7 @@ import {
   CommandList,
 } from "@/components/ui/command";
 import { CommandItem } from "cmdk";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
   data: {
@@ -27,6 +28,8 @@ interface ServerSearchProps {
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -39,6 +42,24 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
+
+  const onClick = ({
+    id,
+    type,
+  }: {
+    id: string;
+    type: "channel" | "member";
+  }) => {
+    setOpen(false);
+
+    if (type === "member") {
+      return router.push(`/servers/${params?.serverId}/conversation/${id}`);
+    }
+
+    if (type === "channel") {
+      return router.push(`/servers/${params.serverId}/channels${id}`);
+    }
+  };
 
   return (
     <>
@@ -65,7 +86,10 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
               <CommandGroup key={label} heading={label}>
                 {data?.map(({ id, icon, name }) => {
                   return (
-                    <CommandItem key={id}>
+                    <CommandItem
+                      key={id}
+                      onSelect={() => onClick({ id, type })}
+                    >
                       {icon}
                       <span>{name}</span>
                     </CommandItem>
