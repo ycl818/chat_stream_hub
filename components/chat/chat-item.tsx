e9ac0ca09fd0fc6313f66418a30271b4744af85f
yichien <kevinyichienlee@gmail.com>
2023-10-17 00:06:11 +0800

    add patch and delete api for message

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -75,8 +75,22 @@ export const ChatItem = ({
     },
   });
 
-  const onSubmit = (values) => {
-    console.log(values);
+  const isLoading = form.formState.isSubmitting;
+
+  // Edit message
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: `${socketUrl}/${id}`,
+        query: socketQuery,
+      });
+
+      await axios.patch(url, values);
+      form.reset();
+      setIsEditing(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -175,6 +189,7 @@ export const ChatItem = ({
                       <FormControl>
                         <div className="relative w-full">
                           <Input
+                            disabled={isLoading}
                             className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75
                           border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0
                           text-zinc-600 dark:text-zinc-200
@@ -187,7 +202,7 @@ export const ChatItem = ({
                     </FormItem>
                   )}
                 />
-                <Button size="sm" variant="primary">
+                <Button disabled={isLoading} size="sm" variant="primary">
                   Save
                 </Button>
               </form>
