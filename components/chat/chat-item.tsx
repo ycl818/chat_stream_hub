4af1d3d197a05851e28142aea2af176f49d7cada
yichien <kevinyichienlee@gmail.com>
2023-10-17 00:21:31 +0800

    add delete message modal and it's api

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -15,6 +15,7 @@ import { cn } from "@/lib/utils";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatItemProps {
   id: string;
@@ -54,7 +55,8 @@ export const ChatItem = ({
   socketQuery,
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
+
+  const { onOpen } = useModal();
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -231,6 +233,12 @@ export const ChatItem = ({
           )}
           <ActionTooltip label="Delete">
             <Trash
+              onClick={() =>
+                onOpen("deleteMessage", {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
               className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600
                 dark:hover:text-zinc-300 transition
               "
